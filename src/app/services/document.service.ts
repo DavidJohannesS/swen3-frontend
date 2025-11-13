5fb2e3743cd3a58c5a2bd48185acc69274384eab
DavidJohannesS <davidjohannesSD@gmail.com>
2025-11-13 21:12:02 +0100

    fix url

--- src/app/services/document.service.ts
+++ src/app/services/document.service.ts
@@ -8,7 +8,7 @@ import { PaperDocument } from '../models/paper-document.model';
 })
 export class DocumentService {
 
-  private readonly BASE_URL = 'http://localhost:8080/api/documents';
+  private readonly BASE_URL = 'http://localhost:8083/api/documents';
 
   constructor(private http: HttpClient) {}
 
